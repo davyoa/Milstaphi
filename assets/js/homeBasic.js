5521b104402677fa9050204bee3fe62aa2bb3d44
David Houde <davidyoa26@gmail.com>
2025-01-09 15:41:10 +0000

    Reuploading files for website
    this time it is may work out of the box

---
+++ assets/js/homeBasic.js
@@ -0,0 +1,46 @@
+const productGrid = document.querySelector('.products-grid');
+
+const productStorage = [
+    {
+        name: 'wheels',
+        imgSrc: '/assets/images/wheels.png'
+    },
+    {
+        name: 'Brake Disc',
+        imgSrc: '/assets/images/brake pad.png'
+    },
+    {
+        name: 'Shock Absorber',
+        imgSrc: '/assets/images/dumper.png'
+    },
+    {
+        name: 'Oil Filter',
+        imgSrc: '/assets/images/cleaner yellow.png'
+    },
+    {
+        name: 'Grille',
+        imgSrc: '/assets/images/bumper mecedes.png'
+    },
+    {
+        name: 'Headlight',
+        imgSrc: '/assets/images/car light.png'
+    }
+]
+const prodFragment = document.createDocumentFragment();
+productStorage.forEach((product) => {
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product-item');
+
+    //just trying somthing out with the code
+    /*const details = document.createElement('div');
+    details.classList.add('item-details');*/
+
+    const productImg =  document.createElement('img');
+    productImg.src = product.imgSrc;
+    productImg.alt = product.name;
+
+    productDiv.appendChild(productImg);
+    prodFragment.appendChild(productDiv);
+});
+
+productGrid.appendChild(prodFragment);
